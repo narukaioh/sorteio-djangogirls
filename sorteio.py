f9de6c9d704cfcbbad5064cb16a1e23e60493536
ClaudiaStrm <s_moura@hotmail.com>
2018-05-09 23:58:15 -0300

    corrige problema com números repetidos

--- sorteio.py
+++ sorteio.py
@@ -1,40 +1,40 @@
-import random
+from random import randint
 import keyboard
 import os
 
-sorteados = []
+lista_sorteados = []
 
-def numeros_sorteados(sorteados):
+def sorteio(total):
+    sorteado = randint(1, total)
+    while sorteado in lista_sorteados:
+        sorteado = randint(1, total)
+    lista_sorteados.append(sorteado)
+    if len(lista_sorteados) == total:
+        print('Acabou o sorteio!')
+    return sorteado
+
+def numeros_sorteados(lista_sorteados):
     print('\n=================')
     print("NUMEROS QUE JA FORAM SORTEADOS: \n")
-    print(sorteados)
+    #lista_sorteados.sort()
+    print(lista_sorteados)
+    print('=================\n')
+    print('Total de números sorteados: ', len(lista_sorteados))
     print('=================\n')
 
+participantes = int(input("\nQuantas participantes? "))
 
 while True:
     if not keyboard.is_pressed('esc'):
-        opcao = int(input("1) DIGITAR NUMERO TOTAL DE PARTICIPANTES\n2) SORTEAR UM NUMERO\n3) Sair \n\n"))
+        opcao = int(input("1) ALTERAR NUMERO TOTAL DE PARTICIPANTES\n2) SORTEAR UM NUMERO\n3) Sair \n\n"))
         os.system('clear')
         if opcao == 1:
             participantes = int(input("\nQuantas participantes? "))
             os.system('clear')
         if opcao == 2:
             try:
-                participantes
-                numero = random.randint(0,participantes)
-                if not numero in sorteados:
-                    sorteados.append(numero)
-                    numeros_sorteados(sorteados)
-                    print("\n NUMERO SORTEADO: "+str(numero)+"\n\n")
-                else:
-                    numeros_sorteados(sorteados)
-                    if (len(sorteados) - 1) == participantes:
-                        os.system("clear")
-                        print("\n\n Acabou sorteio!! \n\n")
-                        break
-                    else:
-                        print("\n Sorteie outro numero! \n\n")
-
+                print('NÚMERO SORTEADO: ', sorteio(participantes))
+                numeros_sorteados(lista_sorteados)
             except NameError:
                 print("\nDigite o TOTAL DE PARTICIPANTES primeiro\n\n")
         if opcao == 3:
